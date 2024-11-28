d569a98335206244af3e22ea00d6742ec7578b8d
irmaDulce <letramentodigital4b@gmail.com>
2024-11-28 19:40:34 -0300

    Relógio pronto, sem cronograma

--- main.ts
+++ main.ts
@@ -1,11 +1,17 @@
-let Horas = 0
-let Minutos = 0
 input.onButtonPressed(Button.A, function () {
-    Horas += Horas + 1
+    Horas += 1
 })
 input.onButtonPressed(Button.B, function () {
-    Minutos = Minutos + 1
+    Minutos += 1
+})
+let Horas = 0
+let Minutos = 0
+loops.everyInterval(60000, function () {
+    Minutos += 1
 })
 basic.forever(function () {
-    basic.showString("" + (Horas))
+    basic.showString("" + Horas + ":" + Minutos)
+})
+loops.everyInterval(3600000, function () {
+    Horas += 1
 })
